88c01131ad6f0459f9e4e0277ff3a4f3156c4ca5
muhammed niyas vp <niyasvp58@gmail.com>
2024-03-16 00:38:08 +0530

    chnge5

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout( ) {
   return (
     <html lang="en">
-      <body className={inter.className}> </body>
+      <body className={inter.className}><Home /> </body>
     </html>
   );
 }
